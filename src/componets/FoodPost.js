87079eaee11f3ad77ca94549f9219c6bc6dca37e
Suniti <sukontaprapun@gmail.com>
2024-10-02 14:02:30 +0700

    test commit

--- src/componets/FoodPost.js
+++ src/componets/FoodPost.js
@@ -2,7 +2,7 @@ function FoodPost() {
     return (
         <div className=''>
             <div></div>
-            <div>
+            <div className="pop__desc">
                 <img className='item__image' src='/images/p1.png' />
                 <h4>xxx</h4>
             </div>
